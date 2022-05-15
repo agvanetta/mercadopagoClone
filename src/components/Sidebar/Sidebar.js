ffe6e32754f4024e81b832064f725784318b110c
Agustin Vanetta <agvanetta@gmail.com>
2022-05-15 01:17:09 -0300

    sidebar_ok

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
-import { BsHouse, BsCash, BsGraphUp, BsPersonDash, BsNewspaper } from "react-icons/bs";
+import { BsHouse, BsPersonCircle, BsHeadset, BsCashCoin,
+  BsBoxArrowRight, BsWallet2, BsCardList, BsArrowLeftRight, BsBasket,
+    BsLink45Deg, BsClipboardData, BsShop, BsPhone } from "react-icons/bs";
+
 
 function Sidebar() {
   return (
@@ -15,17 +18,41 @@ function Sidebar() {
                 />
             </div>
             <div className='sidebar__left__text'>
-                <p>Hola Agustín!</p>
+                <p>Hola Agustín</p>
                 <div>Nivel 4 - Mercado Puntos </div>
             </div>
+      </div>
+        <div className="sidebar__mainpanel__container">
+          <div className="sidebar__mainpanel">
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" style={{color:"#008ad6" , borderLeft:"4.5px solid #008ad6"}} > <div className="sidebar__icons"><BsHouse/></div><span >Inicio</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsPersonCircle/></div><span >Tu perfil</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsHeadset/></div><span >Ayuda</span></div> </Link>
+          </div>
+          <div className="sidebar__border"/>
+          <div className="sidebar__mainpanel">
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsWallet2/></div><span >Tu dinero</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsCardList/></div><span >Actividad</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsArrowLeftRight/></div><span >Enviar dinero</span></div> </Link>
+          </div>
+          <div className="sidebar__border"/>
+          <div className="sidebar__mainpanel">
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsBasket/></div><span >Cobrar en tu local</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsLink45Deg/></div><span >Link de pago</span></div></Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsClipboardData/></div><span >Informes y facturacion</span></div> </Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsCashCoin/></div><span >Planes de suscripción</span></div> </Link>
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsShop/></div><span >Tu negocio</span></div> </Link>
+          </div>
+          <div className="sidebar__border"/>
+
+          <div className="sidebar__mainpanel">
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsBoxArrowRight/></div><span >Salir</span></div></Link>
+          </div>
+          <div className="sidebar__border"/>
+
+          <div className="sidebar__mainpanel">
+            <Link to="/" className="links"><div className="sidebar__mainpanel__options"  > <div className="sidebar__icons"><BsPhone/></div><span id="sidebar__mainpanel__lastOption"><span>Tu celular es tu nueva billetera</span> <p>Descargá la app</p> </span></div></Link>
+          </div>
         </div>
-       <div className="sidebar__mainpanel">
-       <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsHouse/></div><span >Inicio</span></div></Link>
-       <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsCash/></div><span >Tu perfil</span></div></Link>
-       <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsGraphUp/></div><span >Ayuda</span></div> </Link>
-       <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsNewspaper/></div><span >Tu dinero</span></div> </Link>
-       <Link to="/" className="links"><div className="sidebar__mainpanel__options" > <div className="sidebar__icons"><BsPersonDash/></div><span >Actividad</span></div> </Link>
-       </div>
     </div>
   );
 }
